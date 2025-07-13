ba34ca8222a78ae7320f4c1fc83c90447243f700
nikhil9712 <nikhilgulhane52@gmail.com>
2025-07-13 20:23:02 +0530

    New Application Added

---
+++ HibernatePractise/src/main/java/com/learning/hibernate/HibernatePractise/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.learning.hibernate.HibernatePractise;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HibernatePractiseApplication.class);
+	}
+
+}
---
+++ HibernatePractise/src/test/java/com/learning/hibernate/HibernatePractise/HibernatePractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.hibernate.HibernatePractise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernatePractiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
